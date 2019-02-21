94e8b6886a9e22a35a74276fe958be7e38b0cede
Jason Chan <jason741852@gmail.com>
2019-02-20 21:13:43 -0800

    Added client UI and updated dependencies

--- src/client/App.js
+++ src/client/App.js
@@ -1,16 +1,88 @@
+import { Typography, AppBar, TextField, Grid, Button } from "@material-ui/core";
 import React from "react";
 import "./style/style.css";
 
 export default class App extends React.PureComponent
 {
-	render()
+	constructor(props)
+	{
+		super(props);
+
+		this.state =
+			{
+				summerName: ""
+			}
+	}
+
+	render() {
+		return (
+			<>
+				{this.RenderHeader()}
+				<Grid
+					container
+					direction="column"
+					alignItems="center" >
+					{this.RenderInputField()}
+					{this.RenderMatchData()}
+				</Grid>
+			</>
+		);
+	}
+
+	RenderHeader()
 	{
 		return (
-			<div>
-				<h1>
-					{"Hello There"}
-				</h1>
-			</div>
+			<AppBar
+				elevation={0}
+				position="static">
+				<Typography
+					variant={"h1"}>
+					{"League of Lengend Stats App"}
+				</Typography>
+			</AppBar>
 		);
+	}
+
+	RenderHeader()
+	{
+		return (
+			<AppBar
+				elevation={0}
+				position="static">
+				<Typography
+					variant={"h1"}>
+					{"League of Lengend Stats App"}
+				</Typography>
+			</AppBar>
+		);
+	}
+
+	RenderInputField()
+	{
+		return (
+			<>
+				<TextField
+					label={"Enter a Summoner's name"}
+					value={this.state.summerName}
+					onChange={(evt) => this.setState({ summerName: evt.target.value })}
+					autoFocus />
+				<Button
+					variant="contained"
+					color="primary"
+					onClick={() => this.HandleOnSearchClicked()}>
+					{"Search"}
+				</Button>
+			</>
+		)
+	}
+
+	RenderMatchData()
+	{
+		return (null)
+	}
+
+	HandleOnSearchClicked()
+	{
+		// todo
 	}
 }
